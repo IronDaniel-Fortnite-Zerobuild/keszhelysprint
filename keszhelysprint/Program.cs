3837ba83adbdf47aef48b647cd06ef5dd56299fc
IronDániel-Fortnite-ZeroBuild <163414833+IronDaniel-Fortnite-Zerobuild@users.noreply.github.com>
2024-12-09 13:57:00 +0100

    Add project files.

---
+++ keszhelysprint/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+
+namespace keszhelysprint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Versenyzo> Versenyzok = new();
+            foreach (var sor in File.ReadAllLines("eredmenyek.txt"))
+            {
+                Versenyzok.Add(new Versenyzo(sor));
+            }
+            Console.WriteLine($"A versenyt {Versenyzok.Count} versenyzök fejezték be!");
+
+            int elit_junitor = 0;
+            foreach (var v in Versenyzok)
+            {
+                if (v.Kategoria == "elit_junior")
+                {
+                    elit_junior++;
+                }
+                Console.WriteLine($"versenyzök száma az \"elit_junior\"kategoriában:{elit_junior} fö");
+
+
+            }
+
+      
+        }
+        
+
+    }
+
+}
---
+++ keszhelysprint/Versenyzo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace keszhelysprint
+{
+    internal class Versenyzo
+    {
+        public string Nev { get; set; }
+
+        public int Születési_év { get; set; }
+
+        public int Rajtszám { get; set; }
+
+        public string Neme { get; set; }
+
+        public string Életkor { get; set; }
+
+        public string idö1 { get; set; }
+
+        public string idö2 { get; set; }
+
+        public string idö3 { get; set; }
+
+        public string idö4 { get; set; }
+
+        public string idö5 { get; set; }
+        public string Kategoria { get; set; }
+        public Versenyzo(string sor)
+        {
+            string[] sprite = sor.Split(';');
+
+            Nev = sprite[0];
+            Születési_év = int.Parse(sprite[1]);
+            Rajtszám = int.Parse(sprite[2]);
+            Neme = sprite[3];
+            Életkor = sprite[4];
+            idö1 = sprite[5];
+            idö2 = sprite[6];
+            idö3 = sprite[7];
+            idö4 = sprite[8];
+            idö5 = sprite[9];
+            Kategoria = sprite[10];
+        }
+        
+    }
+}
